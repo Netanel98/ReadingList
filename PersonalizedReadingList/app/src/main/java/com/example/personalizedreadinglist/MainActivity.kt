95c0700c96235ceb860fddb341365bf2a85aa167
Netanel98 <netanelsoussan@gmail.com>
2024-08-26 03:15:30 +0300

    Add files via upload

---
+++ PersonalizedReadingList/app/src/main/java/com/example/personalizedreadinglist/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.personalizedreadinglist
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Set up navigation controller with the navHostFragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        return navController.navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
